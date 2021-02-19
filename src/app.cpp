e9489f5da6054008faf8b833372149dd591e8420
Admin <Admin@DESKTOP-FDDIM6N>
2021-02-19 07:19:54 +0200

    Yeah

--- src/app.cpp
+++ src/app.cpp
@@ -2,10 +2,48 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char message = (char*)alloca(length * sizeof(char));
+	}
+	return id;
+}
+
+//the parameters are the source code for the shaders as strings
+//provide OpenGL with the source code for the shaders
+//link and compile the shaders
+//returns an id
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
 int main(void)
 {
 	GLFWwindow* window;
-
 
 	/* Initialize the library */
 	if (!glfwInit())
@@ -35,7 +73,8 @@ int main(void)
 	std::cout << "GLEW version : " << glewGetString(GLEW_VERSION) << std::endl;
 
 	//an array that will act as the position vertex buffer
-	float vert[6] = {
+	float vert[6] = 
+	{
 		-0.5f,  -0.5f,
 		 0.0f,   0.7f,
 		 0.5f,  -0.5f,
